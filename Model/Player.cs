8e11cc07e0791377f4299a3b4ea8791d11ed45a9
KingOfDraughts <barsev@gmail.com>
2015-11-03 23:42:08 +0200

    new changes :
    1. added folder Model which will contain all model classes in my project
    2. added player class.
    need to add more documentation

---
+++ Model/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Draughts.Model
+{
+    public class Player
+    {
+        public string firstName { get; set; }
+    }
+}
